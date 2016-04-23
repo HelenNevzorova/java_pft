310f48d7c6d64da457608b447ffe5a7a65ef6561
HelenNevzorova <helen.nevzorova@gmail.com>
2016-04-23 10:35:46 +0300

    Equation tests added

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by User on 23.04.2016.
+ */
+public class EquationTests {
+
+    @Test
+    public void test0() {
+        Equation equation = new Equation(1, 1, 1);
+        Assert.assertEquals(equation.rootNumber(), 0);
+    }
+
+    @Test
+    public void test1() {
+        Equation equation = new Equation(1, 2, 1);
+        Assert.assertEquals(equation.rootNumber(), 1);
+    }
+
+    @Test
+    public void test2() {
+        Equation equation = new Equation(1, 5, 6);
+        Assert.assertEquals(equation.rootNumber(), 2);
+    }
+
+    @Test
+    public void testLinear() {
+        Equation equation = new Equation(0, 1, 1);
+        Assert.assertEquals(equation.rootNumber(), 1);
+    }
+
+    @Test
+    public void testConstant() {
+        Equation equation = new Equation(0, 0, 1);
+        Assert.assertEquals(equation.rootNumber(), 0);
+    }
+
+    @Test
+    public void testZero() {
+        Equation equation = new Equation(0, 0, 0);
+        Assert.assertEquals(equation.rootNumber(), -1);
+    }
+}
