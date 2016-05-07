d974359e8505e07275426da016c4b6f7369997ea
HelenNevzorova <helen.nevzorova@gmail.com>
2016-05-07 21:08:00 +0300

    Used Hamcrest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by User on 21.04.2016.
@@ -16,23 +18,20 @@ public class ContactModificationTests extends TestBase {
     public void ensurePreconditions() {
         app.goTo().homePage();
         if (app.contact().all().size() == 0) {
-            app.contact().create(new ContactData()
-                    .withFirstName("Elena").withLastName("Nevzorova").withAddress("Earth").withEmail("elena.nevzorova@gmail.com").withGroup("test1"));
+            app.contact().create(new ContactData().withFirstName("Elena").withLastName("Nevzorova")
+                    .withAddress("Earth").withEmail("elena.nevzorova@gmail.com").withGroup("test1"));
         }
     }
 
     @Test
     public void testContactModification() {
-        Set<ContactData> before = app.contact().all();
+        Contacts before = app.contact().all();
         ContactData modifiedContact = before.iterator().next();
-        ContactData contact = new ContactData().withId(modifiedContact.getId())
-                .withFirstName("Elena").withLastName("Nevzorova").withAddress("Earth").withEmail("elena.nevzorova@gmail.com");
+        ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirstName("Elena")
+                .withLastName("Nevzorova").withAddress("Earth").withEmail("elena.nevzorova@gmail.com");
         app.contact().modify(contact);
-        Set<ContactData> after = app.contact().all();
-        Assert.assertEquals(after.size(), before.size());
-
-        before.remove(modifiedContact);
-        before.add(contact);
-        Assert.assertEquals(before, after);
+        Contacts after = app.contact().all();
+        assertEquals(after.size(), before.size());
+        assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
     }
 }
