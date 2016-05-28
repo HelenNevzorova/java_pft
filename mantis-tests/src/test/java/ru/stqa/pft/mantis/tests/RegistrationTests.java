0e71d77326bce2db7a40a84f04d415cadd80d993
HelenNevzorova <helen.nevzorova@gmail.com>
2016-05-28 22:28:01 +0300

    Created unique user name

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -24,9 +24,10 @@ public class RegistrationTests extends TestBase {
 
     @Test
     public void testRegistration() throws IOException, MessagingException {
-        String email = "user1@localhost.localdomain";
-        String user = "user1";
+        long now = System.currentTimeMillis();
+        String user = String.format("user%s", now);
         String password = "password";
+        String email = String.format("user%s@localhost.localdomain", now);
         app.registration().start(user, email);
         List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
