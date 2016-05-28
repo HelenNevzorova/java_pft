5a2f0871d82ebbae55fef0b502f1726676e349ee
HelenNevzorova <helen.nevzorova@gmail.com>
2016-05-28 22:22:38 +0300

    Added Registration test

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -5,3 +5,5 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'elena';
 $g_db_password = 'qvest5';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
